0d4c2e42a41c3b3a967db83cee8e7dd2203177ad
huugooocm <cmhugo307@gmail.com>
2025-10-29 10:17:32 +0100

    file test completed

---
+++ test.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "libreria.h" 
+
+#define DefaultValue 10
+
+int main(int argc, char *argv[]) {
+    int n = DefaultValue;
+    int *funct = NULL;
+
+    if (argc < 2 || argc > 3) {
+        fprintf(stderr, "Usage: %s <function_name> [n]\n", argv[0]);
+        return 1;
+    }
+
+    //first arg: name function
+    funct = argv[1];
+    if (funct[0] != '-'){
+        fprintf(stderr, "Usage: %s must start with '-'", funct);
+        return 1;
+    }
+
+    //second arg (if exists): n lines
+    if argc == 3 {
+        n = atoi(argv[2]);
+        if (n <= 0){
+            fprintf(stderr, "Usage: %d must be optional or greater than 0", n);
+            return 1;
+        }
+    }
+
+    //to execute the function
+    if (strcpm(funct, "-head") == 0){
+        head(n);
+    }else if (strcpm(funct, "-tail") == 0){
+        tail(n);
+    }else if (strcpm(funct, "-longlines") == 0){
+        longlines(n);
+    }else {
+        fprintf(stderr, "Usage: %s must be '-head', '-tail' or '-longlines'", funct);
+        return 1;
+    }
+    
+    return 0;
+    
+}
